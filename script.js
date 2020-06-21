57078e24ff860934473935965ca8d09a30308441
John-Muriu <johnmuriw@gmail.com>
2020-06-21 19:40:17 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// // alert('hey there')
+// console.error("this is an error");
+// console.warn(' this is a worning');
+
+// // variables
+// console.log();
+// console.log();
+// console.error("cobject");
+
+// console.groupEnd();
+// console.table("table");
+
+const name = 'john';
+const age = 34;
+const rating = 4.5;
+
+console.log('my name is ' + name + " i'm " + age + ' years  of age and I have a total rating of' + rating);
+
+//using teplate literals
+const hello = `my name is ${name} , i'm  ${age} ' years  of age and I have a total rating of ${rating}`;
+console.log(hello);
