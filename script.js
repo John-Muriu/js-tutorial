15a6766bd77cf4270583dd293fac3bba4520958c
John-Muriu <johnmuriw@gmail.com>
2020-06-21 21:18:39 +0300

    conantenation

--- script.js
+++ script.js
@@ -10,12 +10,18 @@
 // console.groupEnd();
 // console.table("table");
 
-const name = 'john';
-const age = 34;
-const rating = 4.5;
+// const name = 'john';
+// const age = 34;
+// const rating = 4.5;
 
-console.log('my name is ' + name + " i'm " + age + ' years  of age and I have a total rating of' + rating);
+// console.log('my name is ' + name + " i'm " + age + ' years  of age and I have a total rating of' + rating);
 
-//using teplate literals
-const hello = `my name is ${name} , i'm  ${age} ' years  of age and I have a total rating of ${rating}`;
-console.log(hello);
+// //using teplate literals
+// // const hello = `my name is ${name} , i'm  ${age} ' years  of age and I have a total rating of ${rating}`;
+// console.log(hello);
+
+const num1 = 1000;
+const num2 = 10;
+const sum = `the total sum ${num1 + num2} , difference is ${num1 - num2}, 
+multiplication ${num1 * num2}, division ${num1 / num2}`;
+console.log(sum);
