4141e74982f9e3f21411f91b89689bf177275e8c
John-Muriu <johnmuriw@gmail.com>
2020-06-21 21:54:35 +0300

    arrays

--- script.js
+++ script.js
@@ -24,4 +24,36 @@ const num1 = 1000;
 const num2 = 10;
 const sum = `the total sum ${num1 + num2} , difference is ${num1 - num2}, 
 multiplication ${num1 * num2}, division ${num1 / num2}`;
-console.log(sum);
+// console.log(sum);
+
+// console.log(sum.toLocaleUpperCase(0, 2));
+// console.log(sum.substring(0, 9).toLocaleLowerCase());
+
+//split a string
+// console.log(sum.split(",  "));
+
+// array[array are variable tha hold multiple numbers]
+// new key word means a constructor
+let nums = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0, 'dog');
+const sort = nums.sort();
+sort.push(23);
+sort.unshift('dog');
+// console.log(sort);
+//adding a list to an array
+
+// console.log(Array.isArray(nums));
+
+//objects
+
+const person = {
+    fname: 'john',
+    lname: 'muriu',
+    age: 34,
+    hobbies: ["dancing", "music", "dancing"],
+    address: {
+        street: 'kawangware',
+        city: 'Nairobi',
+        state: 'Ke'
+    }
+}
+console.log(person);
